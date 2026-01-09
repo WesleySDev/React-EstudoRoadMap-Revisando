e5019ff0b17b127f8483e43fd5353f490bb6903c
WesleySDev <wesleydasilvagit@gmail.com>
2026-01-08 21:23:17 -0300

    Refactor: Melhorar a legibilidade do código e os comentários no componente Moda

--- src/hooks/useState/Modal.tsx
+++ src/hooks/useState/Modal.tsx
@@ -5,33 +5,39 @@ import { useState } from 'react'
 
 function Modal() {
   
- const [viewmodal, setViewmodal] = useState(false)
-
+ const [viewmodal, setViewmodal] = useState(false) // aplicação do useState
 
  
   return (
   
    <div className="container">
       <button
-        onClick={() => setViewmodal(true)}
+        onClick={() => setViewmodal(true)} // função que faz o modal aparecer 
       >
         Abrir Modal
       </button>
 
-      {
-        viewmodal &&
-        <div className="modal">
-          <div className="modal__card">
-            <button
-              onClick={() => setViewmodal(false)}
-            >
-              Fechar modal
-            </button>
-            <h3>modal</h3>
-            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Suscipit doloribus incidunt ab facilis alias inventore temporibus aliquam placeat eos beatae labore fugit, illum, sunt tempore laudantium, in omnis asperiores ducimus.</p>
-          </div>
-        </div>
-      }
+     {
+  // Renderiza o modal apenas se viewmodal for true
+  viewmodal && 
+  <div className="modal"> 
+    <div className="modal__card">
+      {/* Botão que fecha o modal ao mudar viewmodal para false */}
+      <button onClick={() => setViewmodal(false)}>
+        Fechar modal
+      </button>
+      <h3>modal</h3>
+      
+      <p>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. 
+        Suscipit doloribus incidunt ab facilis alias inventore 
+        temporibus aliquam placeat eos beatae labore fugit, illum, 
+        sunt tempore laudantium, in omnis asperiores ducimus.
+      </p>
+    </div>
+  </div>
+}
+
 
     </div>
     
