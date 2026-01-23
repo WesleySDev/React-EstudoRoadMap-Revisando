92e9fb398ec63ae8de26a7f2d68632a53a52f0bf
WesleySDev <wesleydasilvagit@gmail.com>
2026-01-23 14:46:24 -0300

    Feat: Adicionar componente Exercio para buscar e exibir produtos da API

--- src/main.tsx
+++ src/main.tsx
@@ -8,7 +8,7 @@ import AlteradorDeTema from './hooks/useState/AlteradorDeTema.tsx'
 import EsconderTexto from './hooks/useState/MostrarEsconderTexto.tsx'
 import ListaSimples from './hooks/useState/ListaSimplesAdicionaItens.tsx'
 import Modal from './hooks/useState/Modal.tsx'
-import Exercio from './hooks/useState/exercicio.tsx'
+import Exercio from './hooks/useState/exercicioApiFetch.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
